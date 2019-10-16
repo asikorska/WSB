af7f860b96eec31760dcbbcc02732afe74902a88
asikorska <56115947+asikorska@users.noreply.github.com>
2019-10-16 19:44:33 +0200

    Add files via upload

---
+++ Podstawy_programowania/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Choinka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj wysokość choinki: ");
+            string h = Console.ReadLine();
+            int h1;
+
+            if (int.TryParse(h, out h1))
+            {
+                if (h1 > 0)
+                {
+                    for (int i = 0; i <= h1; i++)
+                    {
+                        for (int j = 0; j < i; j++)
+                        {
+                            Console.Write("*");
+                         }
+                        Console.Write("\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                }
+            }
+        }
+    }
+}
