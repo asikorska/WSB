2326e55565f8f7ecf5b6f8ca3b94c51c898da316
asikorska <56115947+asikorska@users.noreply.github.com>
2019-10-17 14:23:59 +0200

    Add files via upload

---
+++ Podstawy_programowania/3_duże_litery.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_duże_litery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisz program, który wyświetli duże litery alfabetu
+            //od A do Z i od Z do A z wykorzystaniem funkcji for
+            //np. A,B,C,...,Z.
+
+            char x= 'A';
+            for(int i=0;i<26;i++)
+            {
+                Console.Write(" "+x);           //Console.Write("{0}, "
+                x++;
+            }
+            Console.Write(".");
+            Console.WriteLine();
+
+            char y = 'Z';
+            for (int i = 0; i < 26; i++)
+            {
+                Console.Write(" " + y);
+                y--;
+            }
+            Console.Write(".");
+            Console.WriteLine();
+
+        }
+    }
+}
