f2ca63e123654fd9bbcd4f8228607609fc0f64c5
asikorska <56115947+asikorska@users.noreply.github.com>
2019-10-17 13:54:12 +0200

    Add files via upload

---
+++ Podstawy_programowania/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_tabliczka_mnożenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, row, col;
+            row = 1;
+                do
+                {
+                  col = 1;
+                do
+                {
+                    Console.Write(row * col + "\t");
+                    col++;
+                }
+                while (col <= x);
+                    row++;
+                Console.WriteLine();
+                 }
+                while (row<=x) ;
+           
+        }
+    }
+}
