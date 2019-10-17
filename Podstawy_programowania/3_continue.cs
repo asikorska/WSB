89201d63939dcd192f43b5c06990e117e2cedd9f
asikorska <56115947+asikorska@users.noreply.github.com>
2019-10-17 14:24:35 +0200

    Add files via upload

---
+++ Podstawy_programowania/3_continue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 1;
+            /*for (; j <= 10; j++)
+            {
+                if (j == 2 || j == 5 || j == 7)
+                    continue;
+                else
+                {
+                    Console.Write("{0} ", j);
+                }
+            }*/
+
+          
+            for(;j<=10;j++)
+            {
+                if (j == 2 || j == 5 || j == 7)
+                    break;
+                else
+                {
+                    Console.Write("{0} ",j);
+                }
+            }
+        }
+    }
+}
