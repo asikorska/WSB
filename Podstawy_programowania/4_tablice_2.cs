28b4061d41a823a6180c1ec350dc8f24cf3059e9
asikorska <56115947+asikorska@users.noreply.github.com>
2019-10-24 13:12:15 +0200

    Add files via upload

---
+++ Podstawy_programowania/4_tablice_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_tablice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] liczby = new int[3, 3];
+          
+            
+            for(int i = 0; i < liczby.GetLength(0);i++)
+            {
+                for(int j = 0; j < liczby.GetLength(1);j++)
+                {
+                    liczby[i, j] = i * 3 + j;
+                }
+            }
+
+            for (int i = 0; i < liczby.GetLength(0); i++)
+            {
+                for (int j = 0; j < liczby.GetLength(1); j++)
+                {
+                    Console.Write("{0}\t", liczby[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
