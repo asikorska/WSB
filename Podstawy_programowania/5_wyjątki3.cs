02b4c8f3799fd24e6e298e42f1c836be7a4d5bb7
asikorska <56115947+asikorska@users.noreply.github.com>
2019-11-07 14:11:11 +0100

    Add files via upload

---
+++ Podstawy_programowania/5_wyjątki3.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_wyjątki3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint x = 10;
+            try
+            {
+                Console.WriteLine("Podaj dzień tygodnia (1-5): ");
+                string str = Console.ReadLine();
+                x = uint.Parse(str);
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                Console.WriteLine("x = {0}", x);
+            }
+
+            //####################################################
+
+            switch(x)
+            {
+                case 1:
+                    Console.WriteLine("Poniedziałek");
+                    break;
+                case 2:
+                    Console.WriteLine("Wtorek");
+                    break;
+                case 3:
+                    Console.WriteLine("Środa");
+                    break;
+                case 4:
+                    Console.WriteLine("Czwartek");
+                    break;
+                case 5:
+                    Console.WriteLine("Piątek");
+                    break;
+                case 6:
+                    throw new SystemException("Masz juz weekend, błędne dane ;)");
+                    break;
+                case 7:
+                    throw new SystemException("Masz juz weekend, niedziela!");
+                    break;
+                default:
+                    break;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
