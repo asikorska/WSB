9e0b991bc042c9b8593f3275dc15efaee5804513
asikorska <56115947+asikorska@users.noreply.github.com>
2019-11-13 17:09:24 +0100

    Add files via upload

---
+++ Podstawy_programowania/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _122222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program pokazuje liczby parzyste z przedziału od 20 do 0");
+            int x = 0;
+            for (int i = 20; i >= x; i--)
+            {
+                if (i % 2 == 0)
+                    Console.Write(i + " ");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
