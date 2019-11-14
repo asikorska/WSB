7bb0d83e7421c2ae84dcce81d32c63f2b16912f2
asikorska <56115947+asikorska@users.noreply.github.com>
2019-11-14 14:26:30 +0100

    Add files via upload

---
+++ Podstawy_programowania/6_funkcje3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_funkcje3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            Console.WriteLine("Wartość zmiennej a przed wywołaniem funkcji: {0}",a);
+            increment(ref a);
+            Console.WriteLine("Wartość zmiennej a po wywołaniu funkcji: {0}",a);
+
+
+            Console.ReadKey();
+        }
+
+        static void increment(ref int x)
+        {
+            x += 10;
+            Console.WriteLine("Zmienna x wewnątrz funkcji: {0}",x);
+        }
+    }
+}
---
+++ Podstawy_programowania/6_funkcje4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_funkcje4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            
+            increment(out a);
+            Console.WriteLine("Wartość zmiennej a po wywołaniu funkcji: {0}", a);
+
+
+            Console.ReadKey();
+        }
+
+        static void increment(out int x)
+        {
+            x = 1;
+            x += 10;
+            Console.WriteLine("Zmienna x wewnątrz funkcji: {0}", x);
+        }
+    }
+}
