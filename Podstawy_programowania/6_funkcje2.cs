68cae05dd00f3fa2c466a6ff7661d8acf4f60907
asikorska <56115947+asikorska@users.noreply.github.com>
2019-11-14 14:17:06 +0100

    Add files via upload

---
+++ Podstawy_programowania/6_funkcje2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_funkcje2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisz funkcję, który wyświetli czy liczba jest parzysta/nieparzysta
+            //Funkcja zwraca string (Liczba parzysta / Liczba nieparzysta)
+
+            int x = 10;
+            int y = 7;
+            Console.WriteLine("x to: {0}",checkEven(x));
+            Console.WriteLine("y to: {0}",checkEven(y));
+            Console.WriteLine();
+
+            //Napisz funkcję, który wyświetli sumę cyfr w liczbie
+            Console.WriteLine(Suma(123));
+
+        }
+
+        static string checkEven(int x)
+        {
+            if (x%2==0)
+            {
+              return "Liczba parzysta";
+            }
+            else
+            {
+              return "Liczba nieparzysta";
+            }
+        }
+            
+        static int Suma(int x)
+        {
+            int sum = 0;
+            do
+            {
+                sum = sum + x % 10;
+                x /= 10;
+            } while (x!=0);
+            return sum;
+        }
+    }
+}
