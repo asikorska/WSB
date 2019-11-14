3864698abc1a6e7469d6aa37d9d47f0d1b978fe2
asikorska <56115947+asikorska@users.noreply.github.com>
2019-11-14 14:38:20 +0100

    Add files via upload

---
+++ Podstawy_programowania/6_funkcje5_tablice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace _6_funkcje5_tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[2];
+            tab[0] = 5;
+            tab[1] = 10;
+
+            Console.WriteLine("Wartości tablicy przed wywołaniem funkcji: {0} i {1}", tab[0],tab[1]);
+            Tab1(tab);
+            Console.WriteLine("Wartości tablicy po wywołaniu funkcji: {0} i {1}", tab[0],tab[1]);
+            Console.WriteLine();
+
+            ShowTab(tab);
+
+
+            Console.ReadKey();
+        }
+
+        static void Tab1(int[] tab)
+        {
+            tab[0] = 50;
+            tab[1] = 100;
+            Console.WriteLine("Wartości tablicy wewnątrz funkcji: {0} i {1}", tab[0],tab[1]);
+        }
+
+        static void ShowTab(int[] tab)
+        {
+            Console.WriteLine("Wartości w tablicy: ");
+            foreach (var item in tab)
+            {
+                Console.Write("{0} ",item);
+            }
+        }
+    }
+}
