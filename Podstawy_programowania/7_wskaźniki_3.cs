62d76fc402bf8f5ae1543ee05acb8797d1c9b571
asikorska <56115947+asikorska@users.noreply.github.com>
2019-11-28 14:15:20 +0100

    Add files via upload

---
+++ Podstawy_programowania/7_wskaźniki_3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_wskaźniki_3
+{
+    class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            int[] tab = { 10, 20, 30, 40, 50 };
+
+            fixed (int* x = tab) 
+
+            for(int i=0;i<tab.Length;i++)
+                {
+                    Console.WriteLine("Adres tab[{0}] = {1}, wartość {2}", i, (int)x+i,tab[i]);
+                }
+        }
+    }
+}
