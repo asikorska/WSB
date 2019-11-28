c1ad0f32c1bcdc5d0997e9438d45686883d24804
asikorska <56115947+asikorska@users.noreply.github.com>
2019-11-28 13:53:02 +0100

    Add files via upload

---
+++ Podstawy_programowania/7_wskaźniki_2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_wskaźniki_2
+{
+    class Program
+    {
+        unsafe static void Swap(int* a,int* b)
+        {            
+            int pomoc = *a;
+            *a = *b;
+            *b = pomoc;
+
+        }
+
+        unsafe static void Main(string[] args)
+        {
+            int x = 2;
+            int y = 5;
+
+            Console.WriteLine("Wartości przed zamianą:\nX = {0} \nY = {1}",x,y);
+            Swap(&x, &y);
+            Console.WriteLine("Wartości po zamianie:\nX = {0} \nY = {1}\n",x,y);
+
+            int a = 100;
+            int b = 200;
+
+            int* a1 = &a;
+            int* b1 = &b;
+
+            Console.WriteLine("Wartości przed zamianą:\na = {0} \nb = {1}",a,b);
+            Swap(a1, b1);
+            Console.WriteLine("Wartości po zamianie:\na = {0} \nb = {1}\n",a,b);
+        }
+    }
+}
