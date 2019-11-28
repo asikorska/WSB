82635bb63319849c6fe97559cebb04bba7e0afcb
asikorska <56115947+asikorska@users.noreply.github.com>
2019-11-28 14:21:25 +0100

    Add files via upload

---
+++ Podstawy_programowania/7_wskaźniki_4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_wskaźniki_4
+{
+    class Program
+    {
+        struct Test
+        {
+            public int n;
+        }
+
+        unsafe static void Main(string[] args)
+        {
+            Test x = new Test();
+            Test* p = &x;
+            //p->n = 20;
+
+            (*p).n = 20;
+
+            Console.WriteLine(x.n);
+            Console.ReadKey();
+        }
+    }
+}
